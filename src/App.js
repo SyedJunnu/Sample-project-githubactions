f34822fc315c0154ef3a02ae0a3b39ae1d262c96
SyedJunnu <144713768+SyedJunnu@users.noreply.github.com>
2024-03-11 14:29:29 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Frontend App!</h1>
+      <p>This is a simple React app hosted using GitHub Actions.</p>
+    </div>
+  );
+}
+
+export default App;
